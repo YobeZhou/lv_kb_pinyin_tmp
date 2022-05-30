466b9accfa00b22741e56805b5a9cc96c87d4ebd
Yobe Zhou <33565334+YobeZhou@users.noreply.github.com>
2022-05-30 19:33:27 +0800

    Add files via upload

---
+++ pinyin_search.h
@@ -0,0 +1,12 @@
+﻿#ifndef  _PINYIN_SEARCH_H_
+#define  _PINYIN_SEARCH_H_
+
+
+#define  USER_UNICODE_UTF8      1     //使用 UTF-8 编码
+#define  USER_UNICODE_GBK2312   0   //使用 GBK2312 编码
+
+#define  USER_UNICODE_MODE  USER_UNICODE_UTF8  // USER_UNICODE_UN_UTF8
+
+const char* pinyin_search(char* str_input_str, int* num);
+
+#endif
